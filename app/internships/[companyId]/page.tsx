c778235826d6d65a8889fbdf68b3d806d4fede42
Mehak Garg <mehak.g1205@gmail.com>
2024-12-28 22:07:33 +0530

    Company page

--- app/internships/[companyId]/page.tsx
+++ app/internships/[companyId]/page.tsx
@@ -17,6 +17,10 @@ const companies = [
       { id: '3', name: 'Ishita Narang', year: 2023, branch: 'IT', image: '/we/ishitawe.jpg' },
       { id: '4', name: 'Saumya Johar', year: 2022, branch: 'CSE', image: '/we/saumyawe.jpg' },
       { id: '5', name: 'Bhumika Gupta', year: 2022, branch: 'CSE', image: '/we/bhumikawe.jpg' },
+      { id: '6', name: 'Samiksha Garg', year: 2021, branch: 'IT', image: '/step/samikshastep.jpg'},
+      { id: '39', name: 'Purvi Chaurasia', year: 2022, branch: 'CSE-AI', image: '/uber/purviuber.jpg' },
+      { id: '40', name: 'Radhika Bansal', year: 2022, branch: 'CSEAI', image: '/uber/radhikauber.jpg' },
+      { id: '41', name: 'Astha Vijaywargiya', year: 2023, branch: 'ECEAI', image: '/ln/asthaln.jpg' },
     ]
   },
   { 
@@ -24,30 +28,40 @@ const companies = [
     name: 'Google STEP', 
     logo: '/logo/google logo.png',
     students: [
-      { id: '1', name: 'MAHAK', year: 2025, branch: 'CSE AI', image: '/placeholder.svg?height=200&width=200' },
-      { id: '2', name: 'Bob Smith', year: 4, branch: 'Electrical Engineering', image: '/placeholder.svg?height=200&width=200' },
-      { id: '3', name: 'Bhumika Gupta', year: 2023, branch: 'CSE', image: '/we/bhumikawe.jpg' },
-      { id: '4', name: 'Diana Lee', year: 3, branch: 'Mechanical Engineering', image: '/placeholder.svg?height=200&width=200' },
+      { id: '7', name: 'MAHAK', year: 2025, branch: 'CSE AI', image: '/mahak.jpg' },
+      { id: '8', name: 'Ayushi Arora', year: 2023, branch: 'CSE', image: '/step/ayushistep.jpg' },
+      { id: '9', name: 'Bhumika Gupta', year: 2023, branch: 'CSE', image: '/we/bhumikawe.jpg' },
+      { id: '10', name: 'Jahnavi Malhotra', year: 2022, branch: 'CSE', image: '/step/jahnavistep.jpg' },
+      { id: '11', name: 'Rishita Makde', year: 2024, branch: 'CSE', image: '/anon.jpg' },
+      { id: '12', name: 'Samiksha Garg', year: 2022, branch: 'IT', image: '/step/samikshastep.jpg' },
     ]
   },
   { 
     id: '3', 
-    name: 'DeSHAW', 
+    name: 'DESIS Ascend Educare Mentorship', 
     logo: '/logo/deshaw logo.jpg',
     students: [
-      { id: '5', name: 'Eva Green', year: 3, branch: 'Computer Science', image: '/placeholder.svg?height=200&width=200' },
-      { id: '6', name: 'Frank White', year: 4, branch: 'Electrical Engineering', image: '/placeholder.svg?height=200&width=200' },
-      { id: '7', name: 'Grace Taylor', year: 2, branch: 'Mechanical Engineering', image: '/placeholder.svg?height=200&width=200' },
+      { id: '13', name: 'Gunjan Gupta', year: 2022, branch: 'IT', image: '/desis/gunjandesis.jpg' },
+      { id: '14', name: 'Nikki Gautum', year: 2024, branch: 'CSE', image: '/desis/nikkidesis.jpg' },
+      { id: '15', name: 'Kanika Chouhan', year: 2024, branch: 'CSE', image: '/desis/kanikadesis.jpg' },
+      { id: '16', name: 'Paridhi Jain', year: 2022, branch: 'IT', image: '/desis/paridhidesis.jpg' },
+      { id: '17', name: 'Khushi Mittal', year: 2024, branch: 'ECE-AI', image: '/desis/khushidesis.jpg' },
+      { id: '18', name: 'Gyanvi', year: 2021, branch: 'CSE', image: '/desis/gyanvidesis.jpg' },
+      { id: '38', name: 'Anshika Goel', year: 2024, branch: 'AIML', image: '/ln/anshikaln.jpg' },
     ]
   },
   { 
     id: '4', 
-    name: 'NPX Wit Scholar', 
-    logo: '/logo/npx logo.png',
+    name: 'Amazon ML Summer School', 
+    logo: '/logo/amazon logo.png',
     students: [
-      { id: '5', name: 'Eva Green', year: 3, branch: 'Computer Science', image: '/placeholder.svg?height=200&width=200' },
-      { id: '6', name: 'Frank White', year: 4, branch: 'Electrical Engineering', image: '/placeholder.svg?height=200&width=200' },
-      { id: '7', name: 'Grace Taylor', year: 2, branch: 'Mechanical Engineering', image: '/placeholder.svg?height=200&width=200' },
+      { id: '19', name: 'Jhanavi Malhotra', year: 2024, branch: 'CSEAI', image: '/ml/jhanaviml.jpg' },
+      { id: '20', name: 'Rewa Choudhary', year: 2024, branch: 'ECEAI', image: '/ml/rewaml.jpg' },
+      { id: '21', name: 'Ishani Jain', year: 2022, branch: 'IT', image: '/ml/ishaniml.jpg' },
+      { id: '22', name: 'Saumya Samir', year: 2022, branch: 'CSE', image: '/ml/saumyaml.jpg' },
+      { id: '23', name: 'Sakshi Singh', year: 2024, branch: 'ECEAI', image: '/ml/sakshiml.jpg' },
+      { id: '24', name: 'Arushi Arora', year: 2024, branch: 'AIML', image: '/ml/arushiml.jpg' },
+      
     ]
   },
   { 
@@ -55,9 +69,12 @@ const companies = [
     name: 'Uber STAR', 
     logo: '/logo/uber logo.png',
     students: [
-      { id: '1', name: 'Eva Green', year: 3, branch: 'Computer Science', image: '/placeholder.svg?height=200&width=200' },
-      { id: '2', name: 'Namita Arya', year: 2022, branch: 'CSE', image: '/placeholder.svg?height=200&width=200' },
-      { id: '3', name: 'Chaynika Arora', year: 2022, branch: 'ECE', image: '/uber/chaynikauber.jpg' },
+      { id: '25', name: 'Purvi Chaurasia', year: 2023, branch: 'CSE-AI', image: '/uber/purviuber.jpg' },
+      { id: '26', name: 'Namita Arya', year: 2022, branch: 'CSE', image: '/anon.jpg' },
+      { id: '27', name: 'Radhika Bansal', year: 2023, branch: 'CSEAI', image: '/uber/radhikauber.jpg' },
+      { id: '28', name: 'Payal Narwal', year: 2024, branch: 'CSEAI', image: '/uber/payaluber.jpg' },
+      { id: '29', name: 'Anushree Sharma', year: 2024, branch: 'CSEAI', image: '/uber/anushreeuber.jpg' },
+      { id: '30', name: 'Chaynika Arora', year: 2022, branch: 'ECE', image: '/uber/chaynikauber.jpg' },
     ]
   },
   { 
@@ -65,9 +82,13 @@ const companies = [
     name: 'LinkedIN Coachin', 
     logo: '/logo/linkedin.png',
     students: [
-      { id: '5', name: 'Eva Green', year: 3, branch: 'Computer Science', image: '/placeholder.svg?height=200&width=200' },
-      { id: '6', name: 'Frank White', year: 4, branch: 'Electrical Engineering', image: '/placeholder.svg?height=200&width=200' },
-      { id: '3', name: 'Chaynika Arora', year: 2022, branch: 'ECE', image: '/uber/chaynikauber.jpg' },
+      { id: '31', name: 'Gunjan Gupta', year: 2022, branch: 'IT', image: '/desis/gunjandesis.jpg' },
+      { id: '32', name: 'Tanisha Bansal', year: 2024, branch: 'CSEAI', image: '/ln/tanishaln.jpg' },
+      { id: '33', name: 'Chaynika Arora', year: 2022, branch: 'ECE', image: '/uber/chaynikauber.jpg' },
+      { id: '34', name: 'Samiksha Garg', year: 2022, branch: 'IT', image: '/step/samikshastep.jpg' },
+      { id: '35', name: 'Anshika Goel', year: 2024, branch: 'AIML', image: '/ln/anshikaln.jpg' },
+      { id: '36', name: 'Astha Vijaywargiya', year: 2024, branch: 'ECEAI', image: '/ln/asthaln.jpg' },
+      { id: '37', name: 'Aishwarya Chandra', year: 2024, branch: 'ECEAI', image: '/ln/aishwaryaln.jpg' },
     ]
   },
 ]
