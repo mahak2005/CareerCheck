353b92461ab803a7e007cde09c9c15369df397f5
Mahak <mahakyadav0520@gmail.com>
2025-01-02 21:41:13 +0530

    traffic

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import { Layout } from '@/components/Layout'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
+import Head from "next/head"  // Importing Head component
 
 export const metadata: Metadata = {
   title: "Career Check",
@@ -22,7 +23,16 @@ export default function RootLayout({
 
         {/* Vercel Analytics for visitor and page view tracking */}
         <Analytics />
-        
+
+        {/* Umami Analytics Script */}
+        <Head>
+          <script
+            defer
+            src="https://cloud.umami.is/script.js"
+            data-website-id="421bbba5-4333-449c-ad3e-832797c213dc"
+          ></script>
+        </Head>
+
         {/* Wrap children with your Layout component */}
         <Layout>{children}</Layout>
       </body>
